86cd46a1cf07627b5867f68127acb51092883d6d
Dakota Havel <Havel_Dakota@bah.com>
2021-07-28 14:13:59 -0400

    add job names for days

--- paasJobs/crawler_schedule/crontab.sh
+++ paasJobs/crawler_schedule/crontab.sh
@@ -12,25 +12,25 @@ arg1=/data/gamechanger/gamechanger-data/paasJobs/jobs/configs/crawler_ingest.con
 # min hour dayMonth month dayWeek
 
 # Sunday
-1 1 * * 0 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/sunday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 0 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/sunday.txt" JOB_NAME="Crawler-Sunday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 # Monday
-1 1 * * 1 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/monday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 1 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/monday.txt" JOB_NAME="Crawler-Monday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 # Tuesday
-1 1 * * 2 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/tuesday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 2 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/tuesday.txt" JOB_NAME="Crawler-Tuesday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 # Wednesday
-1 1 * * 3 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/wednesday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 3 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/wednesday.txt" JOB_NAME="Crawler-Wednesday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 # Thursday
-1 1 * * 4 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/thursday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 4 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/thursday.txt" JOB_NAME="Crawler-Thursday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 # Friday
-1 1 * * 5 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/friday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 5 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/friday.txt" JOB_NAME="Crawler-Friday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 # Saturday
-1 1 * * 6 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/saturday.txt" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
+1 1 * * 6 flock -n $flockfile env LOCAL_SPIDER_LIST_FILE="${schedule_dir}/saturday.txt" JOB_NAME="Crawler-Saturday" MAX_OCR_THREADS_PER_FILE=$max_ocr_threads MAX_PARSER_THREADS=$parser_threads SKIP_NEO4J_UPDATE=$skip_neo4j $cmd $arg1
 
 
 
