689fb9857d77e59fbc1425822a742e1a42f29611
Dakota Havel <Havel_Dakota@bah.com>
2021-08-12 10:29:56 -0400

    fixing spiders

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/sorn_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/sorn_spider.py
@@ -3,6 +3,7 @@ from dataPipelines.gc_scrapy.gc_scrapy.items import DocItem
 import json
 import scrapy
 
+
 class SornSpider(GCSpider):
     name = "SORN"
     start_urls = [
@@ -10,8 +11,8 @@ class SornSpider(GCSpider):
     ]
     cac_login_required = False
     doc_type = "SORN"
-    display_org= "Dept. of Defense"
-    display_source= "Federal Registry"
+    display_org = "Dept. of Defense"
+    display_source = "Federal Registry"
 
     def parse(self, response):
         data = json.loads(response.body)
@@ -21,7 +22,8 @@ class SornSpider(GCSpider):
             conditions = conditions + "&conditions[agencies][]=" + item
         notices = "&conditions[type][]=NOTICE"
         page_size = "1000"
-        base_url = "https://www.federalregister.gov/api/v1/documents.json?per_page=" + page_size + "&order=newest&conditions[term]=%22Privacy%20Act%20of%201974%22%20%7C%20%22System%20of%20Records%22"
+        base_url = "https://www.federalregister.gov/api/v1/documents.json?per_page=" + page_size + \
+            "&order=newest&conditions[term]=%22Privacy%20Act%20of%201974%22%20%7C%20%22System%20of%20Records%22"
         next_url = base_url+conditions+notices
         yield scrapy.Request(url=next_url, callback=self.parse_data)
 
@@ -30,7 +32,7 @@ class SornSpider(GCSpider):
         sorns_list = response_json['results']
 
         for sorn in sorns_list:
-            
+
             downloadable_items = [
                 {
                     "doc_type": "pdf",
@@ -39,11 +41,11 @@ class SornSpider(GCSpider):
                 }
             ]
 
-            version_hash_raw_data={
+            version_hash_raw_data = {
                 "item_currency": sorn["publication_date"],
                 "public_inspection": sorn["public_inspection_pdf_url"],
                 "title": sorn["title"],
-                "type":sorn["type"]
+                "type": sorn["type"]
             }
 
             yield DocItem(
@@ -51,8 +53,8 @@ class SornSpider(GCSpider):
                 doc_name="SORN " + sorn["document_number"],
                 doc_title=sorn["title"],
                 doc_num=sorn["document_number"],
-                display_org= "Dept. of Defense",
-                display_doc_type = "SORN",
+                display_org="Dept. of Defense",
+                display_doc_type="Notice",
                 publication_date=sorn["publication_date"],
                 downloadable_items=downloadable_items,
                 version_hash_raw_data=version_hash_raw_data,
