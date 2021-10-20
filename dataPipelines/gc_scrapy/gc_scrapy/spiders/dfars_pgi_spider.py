72a14efbce40659ac2f598390c951a5eda6e48e1
Brandon Herzog <herzog_brandon@bah.com>
2021-10-20 14:45:36 -0400

    scrape publication date

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/dfars_pgi_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/dfars_pgi_spider.py
@@ -1,6 +1,6 @@
 import re
 
-from scrapy.http import TextResponse
+from scrapy.http import TextResponse, Request
 from scrapy.selector import Selector
 
 from dataPipelines.gc_scrapy.gc_scrapy.GCSpider import GCSpider
@@ -9,17 +9,31 @@ from dataPipelines.gc_scrapy.gc_scrapy.items import DocItem
 class DoDSpider(GCSpider):
     name = 'dfars_pgi'
 
-    start_urls = ['https://www.acq.osd.mil/dpap/dars/dfarspgi/current/dfars_pgi.html']
+    start_urls = ['https://www.acq.osd.mil/dpap/dars/dfarspgi/current']
     allowed_domains = ['www.acq.osd.mil']
 
     cac_login_required = False
 
     def parse(self, response: TextResponse):
+        options = response.css('select.tocselect option::text')
+        publication_date = next(match['date'] for match in
+                                map(lambda o: re.match(
+                                    r'Current Version \((?P<date>\d{2}/\d{2}/\d{4})\)', o.get()), options)
+                                if match)
+        meta = {'publication_date': publication_date}
+
+        table_iframe = response.css('iframe[title="DFARS Table"]')
+        table_iframe_src = table_iframe.attrib['src']
+        yield response.follow(table_iframe_src, callback=self.parse_table_iframe, meta=meta)
+
+    def parse_table_iframe(self, response: TextResponse):
+        publication_date = response.meta['publication_date']
+
         table = response.css('#toctable2')
         rows = table.css('tr')
         prev_num = 'CHAPTER 2'  # ?
         prev_title = 'DEFENSE FEDERAL ACQUISITION REGULATION SUPPLEMENT'
-        row: Selector
+        row: Selector                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         for row in rows:
             if row.attrib['class'] == 'rule':
                 part_and_title_raw: str = row.css('td:nth-child(1)::text').get()
@@ -80,6 +94,7 @@ class DoDSpider(GCSpider):
                         doc_name=doc_name,
                         doc_num=doc_num,
                         doc_title=doc_title,
+                        publication_date=publication_date,
                         source_page_url=response.url,
                         downloadable_items=downloadable_items,
                         version_hash_raw_data=version_hash_fields,
@@ -113,6 +128,7 @@ class DoDSpider(GCSpider):
                         doc_name=doc_name,
                         doc_num=doc_num,
                         doc_title=doc_title,
+                        publication_date=publication_date,
                         source_page_url=response.url,
                         downloadable_items=downloadable_items,
                         version_hash_raw_data=version_hash_fields,
