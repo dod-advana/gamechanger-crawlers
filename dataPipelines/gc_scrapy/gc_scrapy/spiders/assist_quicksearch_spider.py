a0b40e28ce3fbdc9521321175b12f9cc68c3b727
Brandon Herzog <herzog_brandon@bah.com>
2021-10-28 13:58:00 -0400

    properly download documents

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/assist_quicksearch_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/assist_quicksearch_spider.py
@@ -2,19 +2,19 @@ from datetime import datetime
 import re
 from urllib.parse import urljoin
 
-from scrapy.http import TextResponse, Request
+from scrapy.http import TextResponse
 from scrapy.selector import Selector
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select, WebDriverWait
 
+
 from dataPipelines.gc_scrapy.gc_scrapy.GCSeleniumSpider import GCSeleniumSpider
 from dataPipelines.gc_scrapy.gc_scrapy.items import DocItem
 
-class DoDSpider(GCSeleniumSpider):
+class AssistQuicksearchSpider(GCSeleniumSpider):
     name = 'assist_quicksearch'
 
     start_urls = ['https://quicksearch.dla.mil/qsSearch.aspx']
@@ -28,26 +28,27 @@ class DoDSpider(GCSeleniumSpider):
         Select(
             driver.find_element_by_css_selector('#DropDownListStatus')
         ).select_by_value("1")  # Active
-
-        search_btn = driver.find_element_by_css_selector('#GetFilteredButton')
+        self.wait_until_css_located(driver, '#DocumentSearchFilters tr:nth-child(2)')
 
         # doc_id_text_box: WebElement = driver.find_element_by_css_selector('#DocumentIDTextBox')  # XXX: delete
         # doc_id_text_box.send_keys('MIL-DTL-17/92')  # XXX: delete
-
+        # doc_id_text_box.send_keys(Keys.ENTER)  # XXX: delete
+        # self.wait_until_css_located(driver, '#DocumentSearchFilters tr:nth-child(3)')  # XXX: delete
+        
+        search_btn = driver.find_element_by_css_selector('#GetFilteredButton')
         search_btn.click()
 
         while True:
             self.wait_until_css_located(driver, '#GV')
             selector = Selector(text=driver.page_source)
-
+            
             doc_rows = selector.css('#GV tr.grid_item,#GV tr.grid_alternate')
             doc_row: Selector
             for doc_row in doc_rows:
                 has_img = doc_row.css('td:nth-child(1) > a::text').get()
                 if has_img != 'Y':  # no available images
                     continue
-                doc_num = doc_row.css('td:nth-child(2)::text').get()  # XXX:
-                # doc_num = ''.join(doc_row.css('td:nth-child(2) *::text').getall())  # XXX: delete
+                doc_num = ''.join(doc_row.css('td:nth-child(2) *::text').getall())
                 doc_link = doc_row.css('td:nth-child(2) > a[title="Go to the Document Details."]::attr(href)').get()
                 meta = {'doc_num': doc_num}
                 yield response.follow(doc_link, callback=self.parse_doc_details, meta=meta)
@@ -74,13 +75,11 @@ class DoDSpider(GCSeleniumSpider):
         publication_date = datetime.strptime(publication_date, '%d-%b-%Y').strftime('%m/%d/%Y')
 
         doc_url = response.css('#GVRevisionHistory tr:nth-child(2) > td > '
-                     'a[title="Click here to view the Document Image"]::attr(href)').get()
+                               'a[title="Click here to view the Document Image"]::attr(href)').get()
         if not doc_url:  # no publicly available download
             return
-        doc_url = re.match(r"javascript:spawnPDFWindow\('(?P<url>.+)',.+\);", doc_url)['url']
-        web_url = urljoin(response.url, doc_url)
-
-        # FIXME: how to actually get download URL?
+        doc_token = re.match(r"javascript:spawnPDFWindow\('\.\/ImageRedirector\.aspx\?token=.+,(?P<token>\d+)\);", doc_url)['token']
+        web_url = urljoin(response.url, f'../../WMX/Default.aspx?token={doc_token}')
 
         version_hash_fields = {
             "item_currency": web_url.split('/')[-1],
@@ -109,4 +108,4 @@ class DoDSpider(GCSeleniumSpider):
         yield pgi_doc_item
 
     def clean_name(self, name):
-        return ' '.join(re.sub(r'[^a-zA-Z0-9. ()-]', '', self.ascii_clean(name).replace('/', '_')).split())
+        return ' '.join(re.sub(r'[^a-zA-Z0-9. ()-_]', '', self.ascii_clean(name).replace('/', '_')).split())
