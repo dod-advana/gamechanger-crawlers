ce1cb4a6287c514ce6603e1cddb464210558aa8d
Gabriella Melki <gabriellamelki@gmail.com>
2022-05-06 00:34:24 -0400

    fix: testing manifest correctness without the access_timestamp key - still need to add manifests for different spiders

--- tests/test_spiders/test_spiders.py
+++ tests/test_spiders/test_spiders.py
@@ -26,7 +26,10 @@ def read_semi_json_file(file_path: Path) -> Iterable[Dict[str, str]]:
                 continue
 
 
-@pytest.mark.parametrize("spider_type,manifest_file", [("test_us_code_spider", "us_code_manifest.json")])
+@pytest.mark.parametrize(
+    "spider_type,manifest_file",
+    [("test_us_code_spider", "test_us_code_manifest.json"), ("us_code_spider", "us_code_manifest.json")],
+)
 def test_spider_manifest_output(spider_type: str, manifest_file: str):
     command = f"{_REPO_ROOT}/.venv/bin/python -m scrapy runspider dataPipelines/gc_scrapy/gc_scrapy/spiders/{spider_type}.py -a download_output_dir=tmp -a previous_manifest_location=tmp/prev_manifest.json -o tmp/output.json"
     if _TEST_TMP_PATH.exists():
@@ -34,15 +37,15 @@ def test_spider_manifest_output(spider_type: str, manifest_file: str):
     _TEST_TMP_PATH.mkdir()
     shutil.copyfile(_TEST_SPIDER_RESOURCES / "prev_manifest.json", _TEST_TMP_PATH / "prev_manifest.json")
 
-    true_manifest_data = list(read_semi_json_file(_TEST_SPIDER_RESOURCES / manifest_file))
+    true_manifest_data = sorted(
+        list(read_semi_json_file(_TEST_SPIDER_RESOURCES / manifest_file)), key=lambda d: d["doc_name"]
+    )
 
     os.system(command)
 
-    manifest_data = list(read_semi_json_file(_TEST_TMP_PATH / "manifest.json"))
-
+    manifest_data = sorted(list(read_semi_json_file(_TEST_TMP_PATH / "manifest.json")), key=lambda d: d["doc_name"])
     for current, true in zip(manifest_data, true_manifest_data):
         _ = current.pop("access_timestamp")
         _ = true.pop("access_timestamp")
-
         assert current == true
     shutil.rmtree(_TEST_TMP_PATH)
