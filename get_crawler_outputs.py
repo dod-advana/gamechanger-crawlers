2ceec5a8c026667621bac8c9fc4b0c21712975af
Gabriella Melki <gabriellamelki@gmail.com>
2022-05-06 15:21:53 -0400

    feat: ability to filter spiders to run through command line

--- get_crawler_outputs.py
+++ get_crawler_outputs.py
@@ -1,6 +1,7 @@
 import shutil
 import subprocess
 from pathlib import Path
+import sys
 from time import time as t
 
 REPO_ROOT = Path(".").resolve()
@@ -10,33 +11,51 @@ DATA_PACKAGE_ROOT = REPO_ROOT / "dataPipelines"
 SCRAPY_PACKAGE_ROOT = DATA_PACKAGE_ROOT / "gc_scrapy"
 SPIDERS_ROOT = SCRAPY_PACKAGE_ROOT / "gc_scrapy" / "spiders"
 
-print("Deleting previous run's output folder")
-if OUTPUT_ROOT.exists():
-    shutil.rmtree(OUTPUT_ROOT)
-OUTPUT_ROOT.mkdir()
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filter_spider_by = sys.argv[1]
+        print(f"Filtering spiders to run by: {filter_spider_by}")
+    else:
+        filter_spider_by = None
 
-print("Getting all spider types and paths")
-spiders_list = [spider_path for spider_path in SPIDERS_ROOT.iterdir() if not spider_path.name.startswith("_")]
+    print("Deleting previous run's output folder")
+    if OUTPUT_ROOT.exists():
+        shutil.rmtree(OUTPUT_ROOT)
+    OUTPUT_ROOT.mkdir()
 
-print("Beginning runspider loop")
-processes = []
-for spider in spiders_list:
-    # get spider name without extensions
-    spider_name = spider.stem
-    output_path = OUTPUT_ROOT / spider_name
-    # create output folders for spiders
-    output_path.mkdir(exist_ok=True, parents=True)
-    # create prev_manifest.json file for each of the spiders
-    (output_path / "prev_manifest.json").touch(exist_ok=True)
-    run_command = f"{str(REPO_ROOT / '.venv/bin/python')} -m scrapy runspider {str(spider)} -a download_output_dir={str(output_path)} -a previous_manifest_location={str(output_path)}/prev_manifest.json -o {str(output_path)}/output.json 2> {str(output_path / (spider_name+'.log'))}"
-    print(run_command)
-    # process = subprocess.Popen(run_command, shell=True, capture_output=True)
-    # processes.append(process)
+    print("Getting all spider types and paths")
+    spiders_list = [
+        (spider_path.stem, spider_path)
+        for spider_path in SPIDERS_ROOT.iterdir()
+        if not spider_path.name.startswith("_")
+    ]
+    spider_names, spider_paths = list(zip(*spiders_list))
 
-# output = [p.wait() for p in processes]
+    if filter_spider_by:
+        if not (filter_spider_by in spider_names):
+            raise ValueError(
+                f"Inputted spider name, {filter_spider_by}, does not exist. Please choose one of the following: {' | '.join(spider_names)}"
+            )
 
-# cleanup
-print("Deleting PDF files for space saving")
-# for pdf_filepath in OUTPUT_ROOT.glob("**/*.pdf"):
-#    pdf_filepath.unlink()
+        spiders_list = [spiders_list[spider_names.index(filter_spider_by)]]
+
+    print("Beginning runspider loop")
+    processes = []
+    for spider_name, spider in spiders_list:
+        output_path = OUTPUT_ROOT / spider_name
+        # create output folders for spiders
+        output_path.mkdir(exist_ok=True, parents=True)
+        # create prev_manifest.json file for each of the spiders
+        (output_path / "prev_manifest.json").touch(exist_ok=True)
+        run_command = f"{str(REPO_ROOT / '.venv/bin/python')} -m scrapy runspider {str(spider)} -a download_output_dir={str(output_path)} -a previous_manifest_location={str(output_path)}/prev_manifest.json -o {str(output_path)}/output.json 2> {str(output_path / (spider_name+'.log'))}"
+        print(run_command)
+        process = subprocess.Popen(run_command, shell=True)
+        processes.append(process)
+
+    output = [p.wait() for p in processes]
+
+    # cleanup
+    print("Deleting PDF files for space saving")
+    for pdf_filepath in OUTPUT_ROOT.glob("**/*.pdf"):
+        pdf_filepath.unlink()
