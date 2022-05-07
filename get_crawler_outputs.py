f65cdce0650a10336570332281e9f951bef45af0
Gabriella Melki <gabriellamelki@gmail.com>
2022-05-06 21:40:38 -0400

    chore: adding click for cmd

--- get_crawler_outputs.py
+++ get_crawler_outputs.py
@@ -3,6 +3,7 @@ import subprocess
 from pathlib import Path
 import sys
 from time import time as t
+import click
 
 REPO_ROOT = Path(".").resolve()
 OUTPUT_ROOT = REPO_ROOT / "testoutput"
@@ -12,12 +13,12 @@ SCRAPY_PACKAGE_ROOT = DATA_PACKAGE_ROOT / "gc_scrapy"
 SPIDERS_ROOT = SCRAPY_PACKAGE_ROOT / "gc_scrapy" / "spiders"
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        filter_spider_by = sys.argv[1]
+@click.command()
+@click.option("--filter-spider-by", "-f", default=None, help="Run specific spiders.")
+@click.option("--log", is_flag=True, default=False, help="Log to a file or not.")
+def hello(filter_spider_by: str, log: bool):
+    if filter_spider_by:
         print(f"Filtering spiders to run by: {filter_spider_by}")
-    else:
-        filter_spider_by = None
 
     print("Deleting previous run's output folder")
     if OUTPUT_ROOT.exists():
@@ -41,6 +42,7 @@ if __name__ == "__main__":
         spiders_list = [spiders_list[spider_names.index(filter_spider_by)]]
 
     print("Beginning runspider loop")
+    log_file_pipe = ""
     processes = []
     for spider_name, spider in spiders_list:
         output_path = OUTPUT_ROOT / spider_name
@@ -48,7 +50,9 @@ if __name__ == "__main__":
         output_path.mkdir(exist_ok=True, parents=True)
         # create prev_manifest.json file for each of the spiders
         (output_path / "prev_manifest.json").touch(exist_ok=True)
-        run_command = f"{str(REPO_ROOT / '.venv/bin/python')} -m scrapy runspider {str(spider)} -a download_output_dir={str(output_path)} -a previous_manifest_location={str(output_path)}/prev_manifest.json -o {str(output_path)}/output.json 2> {str(output_path / (spider_name+'.log'))}"
+        if log or filter_spider_by is None:
+            log_file_pipe = f"2> {str(output_path / (spider_name+'.log'))}"
+        run_command = f"{str(REPO_ROOT / '.venv/bin/python')} -m scrapy runspider {str(spider)} -a download_output_dir={str(output_path)} -a previous_manifest_location={str(output_path)}/prev_manifest.json -o {str(output_path)}/output.json {log_file_pipe}"
         print(run_command)
         process = subprocess.Popen(run_command, shell=True)
         processes.append(process)
@@ -59,3 +63,7 @@ if __name__ == "__main__":
     print("Deleting PDF files for space saving")
     for pdf_filepath in OUTPUT_ROOT.glob("**/*.pdf"):
         pdf_filepath.unlink()
+
+
+if __name__ == "__main__":
+    hello()
