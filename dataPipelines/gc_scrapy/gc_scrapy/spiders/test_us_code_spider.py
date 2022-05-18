cbf48849532229293082522e663cc1bd7b9bbb80
Gabriella Melki <gabriellamelki@gmail.com>
2022-05-17 23:40:35 -0400

    feat: - us code outputs a metadata file for each of the subitems - from what i can tell, it does not check if the file is already downloaded

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/test_us_code_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/test_us_code_spider.py
@@ -20,28 +20,16 @@ def index_containing_substring(the_list, substring):
 
 
 class TestUSCodeSpider(GCSpider):
-    name = "us_code"
+    name = "test_us_code"
     start_urls = ["https://uscode.house.gov/download/download.shtml"]
     doc_type = "Title"
     cac_login_required = False
 
-    custom_settings = {
-        "ITEM_PIPELINES": {
-            "dataPipelines.gc_scrapy.gc_scrapy.pipelines.FileNameFixerPipeline": 50,
-            "dataPipelines.gc_scrapy.gc_scrapy.pipelines.DeduplicaterPipeline": 100,
-            "dataPipelines.gc_scrapy.gc_scrapy.pipelines.AdditionalFieldsPipeline": 200,
-            "dataPipelines.gc_scrapy.gc_scrapy.pipelines.ValidateJsonPipeline": 300,
-        },
-        "FEED_EXPORTERS": {
-            "json": "dataPipelines.gc_scrapy.gc_scrapy.exporters.JsonLinesAsJsonItemExporter",
-        },
-        "DOWNLOADER_MIDDLEWARES": {
-            "dataPipelines.gc_scrapy.gc_scrapy.downloader_middlewares.BanEvasionMiddleware": 100,
-        },
-        # 'STATS_DUMP': False,
-        "ROBOTSTXT_OBEY": False,
-        "LOG_LEVEL": "INFO",
-    }
+    GCSpider.custom_settings["ITEM_PIPELINES"][
+        "dataPipelines.gc_scrapy.gc_scrapy.pipelines.USCodeFileDownloadPipeline"
+    ] = GCSpider.custom_settings["ITEM_PIPELINES"].pop(
+        "dataPipelines.gc_scrapy.gc_scrapy.pipelines.FileDownloadPipeline"
+    )
 
     def parse_data(self, response):
         output_file_name = response.meta["output_file_name"]
@@ -51,14 +39,11 @@ class TestUSCodeSpider(GCSpider):
         downloadable_items = response.meta["downloadable_items"]
         version_hash_raw_data = response.meta["version_hash_raw_data"]
 
-        job_manifest_path = Path(self.download_output_dir, "manifest.json").resolve()
-
         if compression_type:
             file_download_path = Path(self.download_output_dir, output_file_name).with_suffix(f".{compression_type}")
             file_unzipped_path = Path(self.download_output_dir, output_file_name)
         else:
             file_download_path = Path(self.download_output_dir, output_file_name)
-            metadata_download_path = f"{file_download_path}.metadata"
 
         with open(file_download_path, "wb") as f:
             try:
@@ -74,7 +59,6 @@ class TestUSCodeSpider(GCSpider):
                 print("Failed to write file to", file_download_path, "Error:", e)
 
         for unzipped_file in unzipped_files:
-            metadata_download_path = f"{unzipped_file}.metadata"
             version_hash_raw_data.update({"doc_name": unzipped_file.stem})
             item = DocItem(
                 doc_name=unzipped_file.name,
@@ -85,13 +69,6 @@ class TestUSCodeSpider(GCSpider):
                 downloadable_items=downloadable_items,
                 version_hash_raw_data=version_hash_raw_data,
             )
-
-            with open(metadata_download_path, "w") as f:
-                try:
-                    f.write(json.dumps(dict(item)))
-
-                except Exception as e:
-                    print("Failed to write metadata", file_download_path, e)
 
             yield item
 
@@ -122,6 +99,9 @@ class TestUSCodeSpider(GCSpider):
             if not doc_title:
                 continue
 
+            # if not ("42" in doc_num):
+            #     continue
+
             doc_title = doc_title.replace(",", "").replace("'", "")
             doc_name = f"{self.doc_type} {doc_num}{PART}{doc_title}"
 
@@ -136,29 +116,26 @@ class TestUSCodeSpider(GCSpider):
                 href_raw = link.css("::attr(href)").get()
                 web_url = f"https://uscode.house.gov/download/{href_raw}"
 
-                if "42" in link_title:
-                    ext_idx = index_containing_substring(SUPPORTED_URL_EXTENSIONS, link_title)
-                    if ext_idx is not None:
-                        doc_type = SUPPORTED_URL_EXTENSIONS[ext_idx].lower()
-                        compression_type = "zip"
-                        downloadable_items.append(
-                            {"doc_type": doc_type, "web_url": web_url, "compression_type": compression_type}
-                        )
+                ext_idx = index_containing_substring(SUPPORTED_URL_EXTENSIONS, link_title)
+                if ext_idx is not None:
+                    doc_type = SUPPORTED_URL_EXTENSIONS[ext_idx].lower()
+                    compression_type = "zip"
+                    downloadable_items.append(
+                        {"doc_type": doc_type, "web_url": web_url, "compression_type": compression_type}
+                    )
 
-                        meta = {
-                            "output_file_name": f"{doc_name}.{doc_type}",
-                            "doc_type": doc_type,
-                            "compression_type": compression_type,
-                            "doc_num": doc_num,
-                            "downloadable_items": downloadable_items,
-                            "version_hash_raw_data": version_hash_fields,
-                            "web_url": web_url,
-                        }
+                    meta = {
+                        "output_file_name": f"{doc_name}.{doc_type}",
+                        "doc_type": doc_type,
+                        "compression_type": compression_type,
+                        "doc_num": doc_num,
+                        "downloadable_items": downloadable_items,
+                        "version_hash_raw_data": version_hash_fields,
+                        "web_url": web_url,
+                    }
 
-                        yield scrapy.Request(url=web_url, callback=self.parse_data, meta=meta)
+                    yield scrapy.Request(url=web_url, callback=self.parse_data, meta=meta)
+
                 else:
+                    # print("NO DOWNLOADABLE ITEMS", doc_title)
                     continue
-
-            if not len(downloadable_items):
-                # print("NO DOWNLOADABLE ITEMS", doc_title)
-                continue
