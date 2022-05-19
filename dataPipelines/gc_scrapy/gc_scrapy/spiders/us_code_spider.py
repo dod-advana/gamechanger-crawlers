36726ab886b04f7277f980dfa38ed8549e127b55
Gabriella Melki <melki_gabriella@bah.com>
2022-05-18 22:29:32 -0400

    feat: redesigned the request path for us code to handle the nested zipped PDFs

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/us_code_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/us_code_spider.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import re
-from urllib.parse import urljoin
+from pathlib import Path
 from dataPipelines.gc_scrapy.gc_scrapy.items import DocItem
 from dataPipelines.gc_scrapy.gc_scrapy.GCSpider import GCSpider
+from dataPipelines.gc_scrapy.gc_scrapy.utils import unzip_docs_as_needed
 
 PART = " - "
+SUPPORTED_URL_EXTENSIONS = ["PDF"]
+
+
+def index_containing_substring(the_list, substring):
+    for i, s in enumerate(the_list):
+        if s in substring:
+            return i
+    return None
 
 
 class USCodeSpider(GCSpider):
@@ -14,9 +22,56 @@ class USCodeSpider(GCSpider):
     doc_type = "Title"
     cac_login_required = False
 
+    GCSpider.custom_settings["ITEM_PIPELINES"][
+        "dataPipelines.gc_scrapy.gc_scrapy.pipelines.USCodeFileDownloadPipeline"
+    ] = GCSpider.custom_settings["ITEM_PIPELINES"].pop(
+        "dataPipelines.gc_scrapy.gc_scrapy.pipelines.FileDownloadPipeline"
+    )
+
+    def parse_data(self, response):
+        output_file_name = response.meta["output_file_name"]
+        doc_type = response.meta["doc_type"]
+        compression_type = response.meta["compression_type"]
+        doc_num = response.meta["doc_num"]
+        downloadable_items = response.meta["downloadable_items"]
+        version_hash_raw_data = response.meta["version_hash_raw_data"]
+
+        if compression_type:
+            file_download_path = Path(self.download_output_dir, output_file_name).with_suffix(f".{compression_type}")
+            file_unzipped_path = Path(self.download_output_dir, output_file_name)
+        else:
+            file_download_path = Path(self.download_output_dir, output_file_name)
+
+        with open(file_download_path, "wb") as f:
+            try:
+                to_write = self.download_response_handler(response)
+                f.write(to_write)
+                f.close()
+
+                if compression_type:
+                    if compression_type.lower() == "zip":
+                        unzipped_files = unzip_docs_as_needed(file_download_path, file_unzipped_path, doc_type)
+                        print(unzipped_files)
+            except Exception as e:
+                print("Failed to write file to", file_download_path, "Error:", e)
+
+        for unzipped_file in unzipped_files:
+            version_hash_raw_data.update({"doc_name": unzipped_file.stem})
+            item = DocItem(
+                doc_name=unzipped_file.stem,
+                doc_num=doc_num,
+                doc_title=unzipped_file.stem,
+                downloadable_items=downloadable_items,
+                version_hash_raw_data=version_hash_raw_data,
+            )
+
+            yield item
+
     def parse(self, response):
         rows = [el for el in response.css("div.uscitemlist > div.uscitem") if el.css("::attr(id)").get() != "alltitles"]
         prev_doc_num = None
+
+        # for each link in the current start_url
         for row in rows:
             doc_type_num_title_raw = row.css("div:nth-child(1)::text").get()
             is_appendix = row.css("div.usctitleappendix::text").get()
@@ -51,35 +106,26 @@ class USCodeSpider(GCSpider):
                 href_raw = link.css("::attr(href)").get()
                 web_url = f"https://uscode.house.gov/download/{href_raw}"
 
-                if "PDF" in link_title:
+                ext_idx = index_containing_substring(SUPPORTED_URL_EXTENSIONS, link_title)
+                if ext_idx is not None:
+                    doc_type = SUPPORTED_URL_EXTENSIONS[ext_idx].lower()
+                    compression_type = "zip"
                     downloadable_items.append(
-                        {
-                            "doc_type": "pdf",
-                            "web_url": web_url,
-                            "compression_type": "zip",
-                        }
+                        {"doc_type": doc_type, "web_url": web_url, "compression_type": compression_type}
                     )
-                elif "XML" in link_title:
-                    downloadable_items.append(
-                        {
-                            "doc_type": "xml",
-                            "web_url": web_url,
-                            "compression_type": "zip",
-                        }
-                    )
+
+                    meta = {
+                        "output_file_name": f"{doc_name}.{doc_type}",
+                        "doc_type": doc_type,
+                        "compression_type": compression_type,
+                        "doc_num": doc_num,
+                        "downloadable_items": downloadable_items,
+                        "version_hash_raw_data": version_hash_fields,
+                        "web_url": web_url,
+                    }
+
+                    yield scrapy.Request(url=web_url, callback=self.parse_data, meta=meta)
+
                 else:
+                    # print("NO DOWNLOADABLE ITEMS", doc_title)
                     continue
-
-            if not len(downloadable_items):
-                print("NO DOWNLOADABLE ITEMS", doc_title)
-                continue
-
-            yield DocItem(
-                doc_name=doc_name,
-                doc_num=doc_num,
-                doc_title=doc_title,
-                # publication_date=last_action_date,
-                # source_page_url=source_page_url,
-                downloadable_items=downloadable_items,
-                version_hash_raw_data=version_hash_fields,
-            )
