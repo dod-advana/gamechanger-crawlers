74755e1a3691231d4e3aaeae69e7f382c860e00f
Pete Sheurpukdi <sheurpukdi_pete@bah.com>
2022-06-03 11:54:34 -0700

    try init container s3 download for scan task

--- jobs/airflow-jobs/dags/crawler-gc-dev.py
+++ jobs/airflow-jobs/dags/crawler-gc-dev.py
@@ -494,6 +494,15 @@ partition_to_s3_task = PythonOperator(task_id="partition-data",
                                       })
 
 # Run Parallel Scanners
+
+scanner_init_container = k8s.V1Container(
+    name="init-container",
+    image="amazon/aws-cli",
+    command=["bash", "-c"],
+    args=["echo $GC_SCAN_INPUT_PATH && aws s3 cp s3://" + partition_bucket + "/" + partition_directory +
+          "$GC_SCAN_INPUT_PATH $GC_SCAN_INPUT_PATH --recursive"],
+)
+
 # download s3 partitioned files then scan downloaded crawled files then reupload to s3 with metadata files, then upload individual manifests to partition for downstream task to load and combine
 scan_upload = KubernetesPodOperator.partial(namespace="airflow",
                                             image=scanner_image,
@@ -502,11 +511,12 @@ scan_upload = KubernetesPodOperator.partial(namespace="airflow",
                                             task_id="scanupload-task",
                                             get_logs=True,
                                             is_delete_operator_pod=True,
-                                            cmds=["/bin/bash", "-c"],
-                                            arguments=["echo $GC_SCAN_INPUT_PATH && aws s3 cp s3://" + partition_bucket + "/" + partition_directory +
-                                                       "$GC_SCAN_INPUT_PATH $GC_SCAN_INPUT_PATH --recursive && echo 'ran aws s3 cp, now listing' && ls $GC_SCAN_INPUT_PATH && gc scan"],
+                                            cmds=["bash", "-c"],
+                                            arguments=[
+                                                "ls $GC_SCAN_INPUT_PATH && gc scan"],
                                             dag=dag,
                                             do_xcom_push=False,
+                                            init_containers=[scanner_init_container],
                                             annotations={
                                                 "iam.amazonaws.com/role": "advana/k8s/s3.wildcard"},
                                             ).expand(env_vars=XComArg(partition_to_s3_task))
