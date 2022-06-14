4f47d875f9fe483cd9d783e32aac86c8ebd9774d
Matt Styslinger <mstyslinger@gmail.com>
2022-06-14 17:29:20 -0500

    Added code comments for air_force, army_pubs, and army_reserves spiders

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/army_pubs_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/army_pubs_spider.py
@@ -6,72 +6,83 @@ from dataPipelines.gc_scrapy.gc_scrapy.utils import abs_url
 
 
 class ArmySpider(GCSpider):
-    """
-        Parser for Army Publications
-    """
+    '''
+    Class defines the behavior for crawling and extracting text-based documents from the "Army Publishing Directorate" site.
+    This class inherits the 'GCSpider' class from GCSpider.py. The GCSpider class is Gamechanger's implementation of the standard
+    parse method used in Scrapy crawlers in order to return a response.
+    
+    The "class" and its methods = the army_pubs "spider".
+    '''
 
-    name = "army_pubs"
-    allowed_domains = ['armypubs.army.mil']
+    name = "army_pubs" # Spider name (required variable for Scrapy to locate and instantiate the spider)
+    allowed_domains = ['armypubs.army.mil'] # Domains the spider is allowed to crawl
     start_urls = [
         'https://armypubs.army.mil/'
-    ]
+    ] # URL where the spider begins crawling
 
-    file_type = "pdf"
+    file_type = "pdf" # Define filetype for the spider to download
 
-    base_url = 'https://armypubs.army.mil'
-    pub_url = base_url + '/ProductMaps/PubForm/'
+    base_url = 'https://armypubs.army.mil' # Landing page/ base URL
+    pub_url = base_url + '/ProductMaps/PubForm/' # Add extension to landing page base URL to get base URL for document links
     rotate_user_agent = True
 
     def parse(self, response):
+        '''
+        This function compiles relevant document links.
+        '''
         do_not_process = ["/ProductMaps/PubForm/PB.aspx",
-                          "/Publications/Administrative/POG/AllPogs.aspx"]
+                          "/Publications/Administrative/POG/AllPogs.aspx"] # URL stop list
 
         all_hrefs = response.css(
-            'li.usa-nav__primary-item')[2].css('a::attr(href)').getall()
+            'li.usa-nav__primary-item')[2].css('a::attr(href)').getall() # Get all hyperlinks on page
 
-        links = [link for link in all_hrefs if link not in do_not_process]
+        links = [link for link in all_hrefs if link not in do_not_process] # Remove items in URL stop list from hyperlinks list
 
-        yield from response.follow_all(links, self.parse_source_page)
+        yield from response.follow_all(links, self.parse_source_page) # Follow each link and call parse_source_page function for each
 
     def parse_source_page(self, response):
-        table_links = response.css('table td a::attr(href)').extract()
-        yield from response.follow_all([self.pub_url+link for link in table_links], self.parse_detail_page)
+        '''
+        This function grabs links from the raw html for the table on page, calling the parse_detail_page function for the 
+        list of table links.
+        '''
+        table_links = response.css('table td a::attr(href)').extract() # Extract all links in the html table
+        yield from response.follow_all([self.pub_url+link for link in table_links], self.parse_detail_page) # Call parse_detail_page function for each link
 
     def parse_detail_page(self, response):
-        rows = response.css('tr')
-        doc_name_raw = rows.css('span#MainContent_PubForm_Number::text').get()
-        doc_title = rows.css('span#MainContent_PubForm_Title::text').get()
-        doc_num_raw = doc_name_raw.split()[-1]
-        doc_type_raw = doc_name_raw.split()[0]
+        '''
+        This function generates a link and metadata for each document for use by bash download script.
+        '''
+        rows = response.css('tr') # Get table row for document
+        doc_name_raw = rows.css('span#MainContent_PubForm_Number::text').get() # Get 'Number' from table as document name
+        doc_title = rows.css('span#MainContent_PubForm_Title::text').get() # Get document 'Title' from table
+        doc_num_raw = doc_name_raw.split()[-1] # Get numeric portion of document name as doc_num
+        doc_type_raw = doc_name_raw.split()[0] # Get alphabetic portion of document name as doc_type
         publication_date = rows.css(
-            "span#MainContent_PubForm_Date::text").get()
-        dist_stm = rows.css("span#MainContent_PubForm_Dist_Rest::text").get()
+            "span#MainContent_PubForm_Date::text").get() # Get document publication date
+        dist_stm = rows.css("span#MainContent_PubForm_Dist_Rest::text").get() # Get document distribution statment (re: doc accessibility)
         proponent = self.ascii_clean(rows.css(
-            "span#MainContent_PubForm_Proponent::text").get(default=""))
+            "span#MainContent_PubForm_Proponent::text").get(default="")) # Get document "Proponent"
         if dist_stm and (dist_stm.startswith("A") or dist_stm.startswith("N")):
-            # the distribution statement is distribution A or says Not Applicable so anyone can access the information
-            cac_login_required = False
+            cac_login_required = False # The distribution statement is either "A" or "Not Applicable", i.e. anyone can access
         else:
-            # the distribution statement has more restrictions
-            cac_login_required = True
+            cac_login_required = True # The distribution statement has more restrictions
 
-        linked_items = rows.css("div#MainContent_uoicontainer a")
+        linked_items = rows.css("div#MainContent_uoicontainer a") # Get document link in row
         downloadable_items = []
 
-        if not linked_items:
-            # skip over the publication
-            filetype = rows.css("div#MainContent_uoicontainer::text").get()
+        if not linked_items: # Apply generic metadata if no document link
+            filetype = rows.css("div#MainContent_uoicontainer::text").get() ##(**does this assign 'html' as value?)
             if filetype:
                 di = {
                     "doc_type": filetype.strip().lower(),
-                    "web_url": self.base_url,
+                    "web_url": self.base_url, # 'Army Publishing Directorate' base URL as web_url for item
                     "compression_type": None
                 }
                 downloadable_items.append(di)
             else:
                 return
         else:
-            for item in linked_items:
+            for item in linked_items: # Get document-specific metadata
                 di = {
                     "doc_type": item.css("::text").get().strip().lower(),
                     "web_url": abs_url(self.base_url, item.css("::attr(href)").get()).replace(' ', '%20'),
@@ -84,8 +95,9 @@ class ArmySpider(GCSpider):
             "pub_status": rows.css("span#MainContent_PubForm_Status::text").get(),
             "product_status": rows.css("span#MainContent_Product_Status::text").get(),
             "replaced_info": rows.css("span#MainContent_PubForm_Superseded::text").get()
-        }
+        } # Add version hash metadata
 
+        ## Instantiate DocItem class and assign document's metadata values
         yield DocItem(
             doc_name=self.ascii_clean(doc_name_raw),
             doc_title=self.ascii_clean(doc_title),
