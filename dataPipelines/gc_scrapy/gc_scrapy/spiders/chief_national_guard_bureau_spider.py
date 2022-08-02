b3ec34474173db41d24f070b9c94b385f827ffdd
Jon <20669141+takao8@users.noreply.github.com>
2022-08-02 12:54:28 -0400

    fixed chief_national_guard. table needed adjustments for crawl

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/chief_national_guard_bureau_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/chief_national_guard_bureau_spider.py
@@ -8,10 +8,10 @@ class CNGBISpider(GCSpider):
         Parser for Chief National Guard Bureau Instructions
     """
 
-    name = "National_Guard" # Crawler name
-    display_org = "National Guard" # Level 1: GC app 'Source' filter for docs from this crawler
-    data_source = "National Guard Bureau Publications & Forms Library" # Level 2: GC app 'Source' metadata field for docs from this crawler
-    source_title = "Unlisted Source" # Level 3 filter
+    name = "National_Guard"  # Crawler name
+    display_org = "National Guard"  # Level 1: GC app 'Source' filter for docs from this crawler
+    data_source = "National Guard Bureau Publications & Forms Library"  # Level 2: GC app 'Source' metadata field for docs from this crawler
+    source_title = "Unlisted Source"  # Level 3 filter
 
     allowed_domains = ['ngbpmc.ng.mil']
     start_urls = [
@@ -23,7 +23,7 @@ class CNGBISpider(GCSpider):
     rotate_user_agent = True
 
     def parse(self, response):
-        rows = response.css('div.WordSection1 table.MsoNormalTable tbody tr')
+        rows = response.css('div.WordSection1 table tbody tr')
 
         for row in rows:
             href_raw = row.css('td:nth-child(1) a::attr(href)').get()
@@ -45,7 +45,8 @@ class CNGBISpider(GCSpider):
                 }
             ]
 
-            doc_name_raw = row.css('td:nth-child(1) a span::text').get()
+            # a lot of the docs have the space unicode \xa0 in them. replacing it before getting doc_num
+            doc_name_raw = row.css('td:nth-child(1) a::text').get().replace(u'\xa0', ' ')
 
             # one row returns empty for doc_name_raw, so skip that case
             if not doc_name_raw:
