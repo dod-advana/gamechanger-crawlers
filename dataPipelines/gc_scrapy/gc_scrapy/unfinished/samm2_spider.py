2cdfb20576d986ae00bd8e3a26e10bd948d8203d
Segarra <632347@bah.com>
2023-08-04 14:12:40 -0500

    returning pdfs

--- dataPipelines/gc_scrapy/gc_scrapy/unfinished/samm2_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/unfinished/samm2_spider.py
@@ -1,106 +1,69 @@
 import scrapy
-from urllib.parse import urljoin
+import time
 from dataPipelines.gc_scrapy.gc_scrapy.items import DocItem
 from dataPipelines.gc_scrapy.gc_scrapy.GCSpider import GCSpider
 from dataPipelines.gc_scrapy.gc_scrapy.utils import dict_to_sha256_hex_digest, get_pub_date
-import re
 from urllib.parse import urljoin, urlparse
-import time
+import re
 
-class SammSpider(GCSpider):
-    name = "samm_pubs" # Crawler name
+class SammPolicyMemorandaSpider(GCSpider):
+    name = "samm_policy_memoranda" 
     allowed_domains = ["samm.dsca.mil"]
-    start_urls = ["https://samm.dsca.mil/listing/chapters"]
+    start_urls = ["https://samm.dsca.mil/policy-memoranda/PolicyMemoList-All"]
     rotate_user_agent = True
-    doc_type = "SAMM"
+    doc_type = "SAMM Policy Memoranda"
 
     @staticmethod
     def extract_doc_number(doc_title):
         ver_num = 0
         ref_num = 0
 
-        if doc_title is not None:
-            if " Ver " in doc_title:
-                ver_matches = re.findall(r' Ver (\w+)', doc_title)
-                ver_num = ver_matches[0] if ver_matches else 0
-            elif " Version " in doc_title:
-                ver_matches = re.findall(r' Version (\w+)', doc_title)
-                ver_num = ver_matches[0] if ver_matches else 0
-
-            if " Rel " in doc_title:
-                ref_matches = re.findall(r' Rel (\w+)', doc_title)
-                ref_num = ref_matches[0] if ref_matches else 0
-            elif "Release Memo" in doc_title:
-                ref_num = 1
-
         doc_num = f"V{ver_num}R{ref_num}"
         return doc_title, doc_num
 
-
     def parse(self, response):
         base_url = "https://samm.dsca.mil"
-        
-        # Select all detail tags
-        ACCORDION_SELECTOR = '.DSAccordionResponse'
-        accordions = response.css(ACCORDION_SELECTOR)
 
-        for accordion in accordions:
-            ROW_SELECTOR = '.RowContainer a::attr(href)'
-            relative_urls = accordion.css(ROW_SELECTOR).extract()
+        for row in response.xpath('//div[@class="view-content"]//table/tbody/tr'):
+            pm_status_text = row.xpath('td[6]/text()').get()
+            pm_status = pm_status_text.strip() if pm_status_text is not None else ""
+            if pm_status != "Incorporated": 
+                relative_urls = row.xpath('td[2]/a/@href').get()
 
-            for relative_url in relative_urls:
-                absolute_url = urljoin(base_url, relative_url)
+                absolute_url = urljoin(base_url, relative_urls)
                 yield scrapy.Request(absolute_url, callback=self.parse_document_page)
 
-
     def parse_document_page(self, response):
-        rows = response.css('table tbody tr')
-        rows = [a for a in rows if a.css('a::attr(href)').get()]
-        rows = [a for a in rows if a.css('a::attr(href)').get().endswith("pdf")]
+            pdf_link = response.xpath('//div[contains(@class, "PM_PDF_ink")]//a/@href').get()
+            if pdf_link is not None:
+                doc_name = self.ascii_clean(pdf_link.split("/")[-1])
+                doc_title_text = "test"
+                doc_title, doc_num = self.extract_doc_number(doc_title_text)
 
-        for row in rows:
-            try:
-                href_raw = row.css('a::attr(href)').get()
-                doc_title_text = row.css('a::text').get().strip() 
-                publication_date_raw = row.css('td:nth-child(2)::text').get()
-                doc_title = self.ascii_clean(doc_title_text).replace("/ ", " ").replace("/", " ")
-                publication_date = self.ascii_clean(publication_date_raw)
-                doc_title, doc_num = self.extract_doc_number(doc_title)
-                
-                # Extract file name from href and use it as doc_name
-                doc_name = href_raw.split("/")[-1]
-                doc_name = doc_title_text.replace(" ", "_")  
-                doc_name = self.ascii_clean(doc_name)
-
+                doc_name = self.ascii_clean(doc_title_text.replace(" ", "_"))
                 display_doc_type = "SAMM"
 
-                # Generate the unique doc name
-                doc_name, doc_title, unique_doc_name = self.generate_unique_doc_name({'doc_name': doc_name, 'doc_title': doc_title})
-
-                file_type = self.get_href_file_extension(href_raw)
-                web_url = self.ensure_full_href_url(href_raw, response.url)
-
+                doc_name, doc_title, unique_doc_name = self.generate_unique_doc_name({'doc_name': doc_name, 'doc_title': "test"})
+                file_type = self.get_href_file_extension(pdf_link)
+                web_url = self.ensure_full_href_url(pdf_link, response.url)
+                
                 fields = {
                     'doc_name': doc_name,
                     'unique_doc_name': unique_doc_name,
                     'doc_num': doc_num,
                     'doc_title': doc_title,
                     'doc_type': self.doc_type,
-                    'display_doc_type':display_doc_type,
-                    'file_type':file_type,
+                    'display_doc_type': display_doc_type,
+                    'file_type': file_type,
                     'cac_login_required': False,
                     'download_url': web_url,
-                    'source_page_url':response.url,
-                    'publication_date': publication_date
+                    'source_page_url': response.url,
+                    'publication_date': None
                 }
 
-                ## Instantiate DocItem class and assign document's metadata values
                 doc_item = self.populate_doc_item(fields)
 
                 yield doc_item
-
-            except Exception as e:
-                self.logger.error(f"Error processing row {row}: {e}")
 
     def generate_unique_doc_name(self, data):
         # Clean doc_name
@@ -121,7 +84,6 @@ class SammSpider(GCSpider):
         unique_doc_name = re.sub(r'[\W_\.]+$', '', unique_doc_name)
 
         return doc_name, doc_title, unique_doc_name
-
 
     def populate_doc_item(self, fields):
         #
