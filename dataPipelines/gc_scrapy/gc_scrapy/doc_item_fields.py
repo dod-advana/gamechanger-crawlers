ab6743cb8312747b6702f644da34a5cd0c457e84
Matthew Kersting <kersting_matthew@bah.com>
2024-05-02 20:34:57 +0000

    new div selection for updated site

--- dataPipelines/gc_scrapy/gc_scrapy/doc_item_fields.py
+++ dataPipelines/gc_scrapy/gc_scrapy/doc_item_fields.py
@@ -38,16 +38,18 @@ class DocItemFields:
         self.file_ext = file_ext
         self.display_title = doc_type + " " + doc_num + ": " + doc_title
 
-    def get_version_hash_fields(self) -> dict:
-        """Returns a dict of the fields used for hashing"""
-        return {
+        self.hash_fields = {
             "doc_name": self.doc_name,
             "doc_num": self.doc_num,
             "publication_date": self.publication_date,
             "download_url": self.download_url,
-            "display_title": self.display_title,
+            "display_title": self.display_title, 
         }
 
+    def set_version_hash_field(self, key: str, value: str) -> dict:
+        """Sets a new field or updates an old one in the dict used for hashing"""
+        self.hash_fields[key] = value
+        
     def set_display_name(self, name: str) -> None:
         """Update display name for DocItemFields instance"""
         self.display_title = name
@@ -70,8 +72,7 @@ class DocItemFields:
         display_source = data_source + " - " + source_title
         is_revoked = False
         source_fqdn = urlparse(self.source_page_url).netloc
-        version_hash_fields = self.get_version_hash_fields()
-        version_hash = dict_to_sha256_hex_digest(version_hash_fields)
+        version_hash = dict_to_sha256_hex_digest(self.hash_fields)
 
         return DocItem(
             doc_name=self.doc_name,
@@ -86,7 +87,7 @@ class DocItemFields:
             source_page_url=self.source_page_url,
             source_fqdn=source_fqdn,
             download_url=self.download_url,
-            version_hash_raw_data=version_hash_fields,
+            version_hash_raw_data=self.hash_fields,
             version_hash=version_hash,
             display_org=display_org,
             data_source=data_source,
