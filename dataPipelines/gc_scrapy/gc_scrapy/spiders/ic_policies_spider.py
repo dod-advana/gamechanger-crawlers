f44c46fef505d61b05b5d7d113eeb7e5fcb81738
Matthew Kersting <52083309+matthew-kersting@users.noreply.github.com>
2024-05-16 13:55:00 -0400

    Patch IC Spider (#230)
    * avoid redirection from start page
    
    * use DocItemFields class
    
    * pick up legal reference book
    
    * refactor and clean up implementation
    
    * use fedora and match chromedriver version on dev
    
    ---------
    
    Co-authored-by: Matthew Kersting <kersting_matthew@bah.com>

--- dataPipelines/gc_scrapy/gc_scrapy/spiders/ic_policies_spider.py
+++ dataPipelines/gc_scrapy/gc_scrapy/spiders/ic_policies_spider.py
@@ -1,171 +1,177 @@
 import re
 import bs4
-from dataPipelines.gc_scrapy.gc_scrapy.GCSpider import GCSpider
-from dataPipelines.gc_scrapy.gc_scrapy.items import DocItem
-from dataPipelines.gc_scrapy.gc_scrapy.utils import abs_url
+import time
+from selenium.webdriver import Chrome
 
-from urllib.parse import urljoin, urlparse
-from datetime import datetime
-from dataPipelines.gc_scrapy.gc_scrapy.utils import dict_to_sha256_hex_digest, get_pub_date
+from dataPipelines.gc_scrapy.gc_scrapy.doc_item_fields import DocItemFields
+from dataPipelines.gc_scrapy.gc_scrapy.GCSeleniumSpider import GCSeleniumSpider
+from dataPipelines.gc_scrapy.gc_scrapy.utils import abs_url, parse_timestamp
 
-class IcPoliciesSpider(GCSpider):
-    name = 'ic_policies' # Crawler name
 
+class IcPoliciesSpider(GCSeleniumSpider):
+    """
+    As of 05/15/2024
+    crawls https://www.dni.gov/index.php/what-we-do/ic-related-menus/ic-related-links/intelligence-community-directives for 70 pdfs (doc_type = ICD)
+    and https://www.dni.gov/index.php/what-we-do/ic-related-menus/ic-related-links/intelligence-community-policy-guidance for 29 pdfs (doc_type = ICPG)
+    and https://www.dni.gov/index.php/what-we-do/ic-related-menus/ic-related-links/intelligence-community-policy-memorandums for 5 pdfs (doc_type = ICPG)
+    and https://www.dni.gov/index.php/who-we-are/organizations/ogc/ogc-related-menus/ogc-related-content/ic-legal-reference-book for 1 pdf (doc_type = ICLR)
+    """
+
+    # Crawler name
+    name = "ic_policies"
+    # Level 1: GC app 'Source' filter for docs from this crawler
+    display_org = "Intelligence Community"
+    # Level 2: GC app 'Source' metadata field for docs from this crawler
+    data_source = "Office of Director of National Intelligence"
+    # Level 3 filter
+    source_title = "Unlisted Source"
+
+    allowed_domains = ["www.dni.gov"]
+    base_url = "https://www.dni.gov"
+    links_path = "index.php/what-we-do/ic-related-menus/ic-related-links"
     start_urls = [
-        'https://www.dni.gov/index.php/what-we-do/ic-policies-reports/'
+        f"{base_url}/{links_path}/intelligence-community-directives",
+        f"{base_url}/{links_path}/intelligence-community-policy-guidance",
+        f"{base_url}/{links_path}/intelligence-community-policy-memorandums",
+        f"{base_url}/{links_path}/intelligence-community-policy-framework-on-commercially-available-information",
+        f"{base_url}/index.php/who-we-are/organizations/ogc/ogc-related-menus/ogc-related-content/ic-legal-reference-book",
     ]
+
     rotate_user_agent = True
+    randomly_delay_request = True
+    headers = {
+        "authority": "www.dni.gov",
+        "referer": "https://www.dni.gov/",
+        "accept": "text/html,application/xhtml+xml;q=0.9,image/webp,*/*;q=0.8;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+        "sec-ch-ua": '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+    }
+    custom_settings = {
+        **GCSeleniumSpider.custom_settings,
+        "DOWNLOAD_TIMEOUT": 7.0,
+        "DOWNLOAD_DELAY": 5,
+        "AUTOTHROTTLE_ENABLED": True,
+        "AUTOTHROTTLE_START_DELAY": 1,
+        "AUTOTHROTTLE_MAX_DELAY": 10,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 1,
+    }
 
     @staticmethod
-    def get_display_doc_type(doc_type):
-        """This function returns value for display_doc_type based on doc_type -> display_doc_type mapping"""
-        display_type_dict = {
-        "icd": 'Directive',
-        "icpg": 'Guide',
-        "icpm": 'Manual'
-        }
-        if doc_type.lower() in display_type_dict.keys():
+    def get_display_doc_type(doc_type: str) -> str:
+        """Returns value for display_doc_type based on doc_type -> display_doc_type mapping"""
+        display_type_dict = {"icd": "Directive", "icpg": "Guide", "icpm": "Manual"}
+        if doc_type.lower() in display_type_dict:
             return display_type_dict[doc_type.lower()]
-        else:
-            return "Document"
+        return "Document"
+
+    @staticmethod
+    def get_doc_type(url: str) -> str:
+        """Set doc type from URL"""
+        if url.endswith("directives"):
+            return "ICD"
+        if url.endswith("guidance"):
+            return "ICPG"
+        if url.endswith("memorandums"):
+            return "ICPM"
+        return "ICLR"
+
+    @staticmethod
+    def get_policy_doc_info(data: str) -> tuple:
+        """Get doc info for ICD, ICPG, and ICPM docs"""
+        name_pattern = re.compile(r"^[A-Z]*\s\d*.\d*.\d*.\d*\s")
+        names = re.findall(name_pattern, data)
+        parsed_text = names[0]
+        parsed_name = parsed_text.split(" ")
+
+        doc_name = " ".join(parsed_name[:2])
+        doc_num = parsed_name[1]
+        doc_title = re.sub(parsed_text, "", data)
+        return doc_name, doc_num, doc_title
+
+    @staticmethod
+    def get_legal_doc_info(data: str) -> tuple:
+        """Get doc info for legal reference"""
+        split_data = data.split(" ")
+        doc_name = " ".join(split_data[:-1])
+        doc_num = split_data[-1]
+        doc_title = doc_name
+        return doc_name, doc_num, doc_title
+
+    @staticmethod
+    def is_cac_required(pdf_url: str, doc_title: str) -> bool:
+        """Return true if cac is required for access"""
+        cac_required = ["CAC", "PKI certificate required", "placeholder", "FOUO"]
+        return (
+            True
+            if any(x in pdf_url for x in cac_required)
+            or any(x in doc_title for x in cac_required)
+            else False
+        )
 
     def parse(self, response):
-        base_url = 'https://www.dni.gov'
-        links = response.css('div[itemprop="articleBody"]').css('ul')[
-            0].css('li')[:-1]
-        full_links = [base_url + l.css('a::attr(href)').get() for l in links]
-        yield from response.follow_all(full_links, self.parse_documents)
+        """Parses doc items out of IC Policies and Directives site"""
+        driver: Chrome = response.meta["driver"]
 
-    def parse_documents(self, response):
-        """Parse document objects from page of text"""
-        page_url = response.url
-        # parse html response
-        base_url = 'https://www.dni.gov'
-        soup = bs4.BeautifulSoup(response.body, features="html.parser")
-        div = soup.find('div', attrs={'itemprop': 'articleBody'})
-        pub_list = div.find_all('p')
+        for page_url in self.start_urls:
+            driver.get(page_url)
+            time.sleep(5)
 
-        # set policy type
-        if page_url.endswith('directives'):
-            doc_type = 'ICD'
-        elif page_url.endswith('guidance'):
-            doc_type = 'ICPG'
-        elif page_url.endswith('memorandums'):
-            doc_type = 'ICPM'
-        else:
-            doc_type = 'ICLR'
+            # parse html response
+            div = bs4.BeautifulSoup(driver.page_source, features="html.parser").find(
+                "div", attrs={"itemprop": "articleBody"}
+            )
 
-        # iterate through each publication
-        cac_required = ['CAC', 'PKI certificate required',
-                        'placeholder', 'FOUO']
-        for row in pub_list:
+            # set policy type
+            doc_type = self.get_doc_type(page_url)
 
-            # skip empty rows
-            if row.a is None:
-                continue
+            # iterate through each publication
+            for row in div.find_all("p"):
+                # skip empty rows
+                if row.a is None:
+                    continue
 
-            data = re.sub(r'\u00a0', ' ', row.text)
-            link = row.a['href']
+                data = re.sub(r"\u00a0", " ", row.text)
+                pdf_url = abs_url(self.base_url, row.a["href"])
 
-            # patterns to match
-            name_pattern = re.compile(r'^[A-Z]*\s\d*.\d*.\d*.\d*\s')
+                # patterns to match
+                try:
+                    doc_name, doc_num, doc_title = self.get_policy_doc_info(data)
+                except IndexError:
+                    doc_name, doc_num, doc_title = self.get_legal_doc_info(data)
 
-            parsed_text = re.findall(name_pattern, data)[0]
-            parsed_name = parsed_text.split(' ')
-            doc_name = ' '.join(parsed_name[:2])
-            doc_num = parsed_name[1]
-            doc_title = re.sub(parsed_text, '', data)
+                # extract publication date from the pdf url
+                matches = re.findall(r"\((.+)\)", pdf_url.replace("%20", "-"))
+                publication_date = matches[-1] if len(matches) > 0 else None
 
-            pdf_url = abs_url(base_url, link)
-            
+                fields = DocItemFields(
+                    doc_name=doc_name.strip(),
+                    doc_title=doc_title,
+                    doc_num=doc_num,
+                    doc_type=doc_type,
+                    publication_date=parse_timestamp(publication_date),
+                    cac_login_required=self.is_cac_required(pdf_url, doc_title),
+                    source_page_url=page_url.strip(),
+                    downloadable_items=[
+                        {
+                            "doc_type": "pdf",
+                            "download_url": pdf_url,
+                            "compression_type": None,
+                        }
+                    ],
+                    download_url=pdf_url,
+                    file_ext="pdf",
+                    display_doc_type=self.get_display_doc_type(doc_type),
+                )
 
-            # extract publication date from the pdf url
-            matches = re.findall(r'\((.+)\)', pdf_url.replace('%20', '-'))
-            publication_date = matches[-1] if len(matches) > 0 else None
-
-            # set boolean if CAC is required to view document
-            cac_login_required = True if any(x in pdf_url for x in cac_required) \
-                or any(x in doc_title for x in cac_required) else False
-
-
-            fields = {
-                'doc_name': doc_name.strip(),
-                'doc_num': doc_num,
-                'doc_title': doc_title,
-                'doc_type': doc_type,
-                'cac_login_required': cac_login_required,
-                'download_url': pdf_url,
-                'source_page_url':page_url.strip(),
-                'publication_date': publication_date
-            }
-            ## Instantiate DocItem class and assign document's metadata values
-            doc_item = self.populate_doc_item(fields)
-        
-            yield doc_item
-        
-
-
-    def populate_doc_item(self, fields):
-        '''
-        This functions provides both hardcoded and computed values for the variables
-        in the imported DocItem object and returns the populated metadata object
-        '''
-        display_org = "Intelligence Community" # Level 1: GC app 'Source' filter for docs from this crawler
-        data_source = "Office of Director of National Intelligence" # Level 2: GC app 'Source' metadata field for docs from this crawler
-        source_title = "Unlisted Source" # Level 3 filter
-
-        doc_name = fields['doc_name']
-        doc_num = fields['doc_num']
-        doc_title = fields['doc_title']
-        doc_type = fields['doc_type']
-        cac_login_required = fields['cac_login_required']
-        download_url = fields['download_url']
-        publication_date = get_pub_date(fields['publication_date'])
-
-        display_doc_type = self.get_display_doc_type(doc_type)
-        display_source = data_source + " - " + source_title
-        display_title = doc_type + " " + doc_num + ": " + doc_title
-        is_revoked = False
-        source_page_url = fields['source_page_url']
-        source_fqdn = urlparse(source_page_url).netloc
-
-        downloadable_items = [{
-                "doc_type": "pdf",
-                "download_url": download_url,
-                "compression_type": None,
-            }]
-        file_ext = downloadable_items[0]["doc_type"]
-        ## Assign fields that will be used for versioning
-        version_hash_fields = {
-            "doc_name":doc_name,
-            "doc_num": doc_num,
-            "publication_date": publication_date,
-            "download_url": download_url,
-            "display_title": display_title
-        }
-
-        version_hash = dict_to_sha256_hex_digest(version_hash_fields)
-
-        return DocItem(
-                    doc_name = doc_name,
-                    doc_title = doc_title,
-                    doc_num = doc_num,
-                    doc_type = doc_type,
-                    display_doc_type = display_doc_type, #
-                    publication_date = publication_date,
-                    cac_login_required = cac_login_required,
-                    crawler_used = self.name,
-                    downloadable_items = downloadable_items,
-                    source_page_url = source_page_url, #
-                    source_fqdn = source_fqdn, #
-                    download_url = download_url, #
-                    version_hash_raw_data = version_hash_fields, #
-                    version_hash = version_hash,
-                    display_org = display_org, #
-                    data_source = data_source, #
-                    source_title = source_title, #
-                    display_source = display_source, #
-                    display_title = display_title, #
-                    file_ext = file_ext, #
-                    is_revoked = is_revoked, #
+                yield fields.populate_doc_item(
+                    display_org=self.display_org,
+                    data_source=self.data_source,
+                    source_title=self.source_title,
+                    crawler_used=self.name,
                 )
